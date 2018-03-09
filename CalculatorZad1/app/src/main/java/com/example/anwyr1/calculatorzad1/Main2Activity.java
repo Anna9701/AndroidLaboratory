e61ba96bea44c93e89993b7d4acdc01c565929d9
Anna Wyrwal <202340@edu.p.lodz.pl>
2018-03-09 15:18:20 +0100

    init

---
+++ CalculatorZad1/app/src/main/java/com/example/anwyr1/calculatorzad1/Main2Activity.java
@@ -0,0 +1,45 @@
+package com.example.anwyr1.calculatorzad1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button exitButton = (Button) findViewById(R.id.button4);
+        Button aboutButton = (Button) findViewById(R.id.button3);
+        Button advancedButton = (Button) findViewById(R.id.button2);
+        Button simpleButton = (Button) findViewById(R.id.button);
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+    }
+
+}
