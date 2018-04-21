65a1487c953ee1fe9ac67e9bee6aade9a0364c0c
Anna Wyrwal <202340@edu.p.lodz.pl>
2018-04-21 17:20:26 +0200

    Working astro calendar, TODO: test on tablet

--- AstronomicWeatherApp/app/src/main/java/com/example/anwyr1/astronomicweatherapp/MainActivity.java
+++ AstronomicWeatherApp/app/src/main/java/com/example/anwyr1/astronomicweatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.anwyr1.astronomicweatherapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.astrocalculator.AstroCalculator;
+import com.astrocalculator.AstroDateTime;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
@@ -19,6 +23,10 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     private Timer timer;
     private TimerTask timerTask;
+    private static final int MilisecondsInMinute = 1000;
+    private static AstroCalculator astroCalculator;
+    private static AstroCalculator.Location location;
+    private static AstroDateTime astroDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setupCurrentTimeAndLocalization();
-
+        setupAstronimicData();
     }
 
     private void setupCurrentTimeAndLocalization() {
@@ -41,15 +49,79 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Date currentTime = Calendar.getInstance().getTime();
-                        textView.setText(String.format("%s \n%s \n%s", latitude, longitude, currentTime.toString()));
+                        textView.setText(String.format("%s, %s \n%s", latitude, longitude, currentTime.toString()));
                     }
                 });
             }
         }, 0, 1000);
     }
 
-    private void setupAstronimicData() {
+    private void updateAstroCalendar() {
+        final String latitude = SettingsActivity.getFromSettings("latitude", this);
+        final String longitude = SettingsActivity.getFromSettings("longitude", this);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        int hour = Calendar.getInstance().get(Calendar.HOUR);
+        int minute = Calendar.getInstance().get(Calendar.MINUTE);
+        int second = Calendar.getInstance().get(Calendar.SECOND);
+        int timezeoneOffset = Calendar.getInstance().get(Calendar.ZONE_OFFSET);
+        astroDateTime = new AstroDateTime(year, month, day, hour, minute, second, timezeoneOffset, false);
+        location = new AstroCalculator.Location(Double.parseDouble(latitude),
+                Double.parseDouble(longitude));
+        astroCalculator = new AstroCalculator(astroDateTime, location);
+    }
 
+    private void setupAstronimicData() {
+        updateAstroCalendar();
+        int time = Integer.parseInt(SettingsActivity.getFromSettings("sync_frequency", this));
+        time *= MilisecondsInMinute;
+        final Context context = this;
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new TimerTask() {
+                    @Override
+                    public void run() {
+                        updateAstroCalendar();
+                        loadSunRelatedData();
+                        loadMoonRelatedData();
+                    }
+                });
+            }
+        }, 0, time);
+    }
+
+    private void loadSunRelatedData() {
+        AstroCalculator.SunInfo sunInfo = astroCalculator.getSunInfo();
+        final TextView sunriseDate = findViewById(R.id.sunriseTime);
+        final TextView sunriseAzimuth = findViewById(R.id.sunriseAzimuth);
+        final TextView sunsetDate = findViewById(R.id.sunsetTime);
+        final TextView sunsetAzimuth = findViewById(R.id.sunsetAzimuth);
+        final TextView civilSunrise = findViewById(R.id.civilSunrise);
+        final TextView civilSunset = findViewById(R.id.civilSunset);
+        sunriseDate.setText(sunInfo.getSunrise().toString());
+        sunriseAzimuth.setText(String.valueOf(sunInfo.getAzimuthRise()));
+        sunsetDate.setText(sunInfo.getSunset().toString());
+        sunsetAzimuth.setText(String.valueOf(sunInfo.getAzimuthSet()));
+        civilSunrise.setText(sunInfo.getTwilightMorning().toString());
+        civilSunset.setText(sunInfo.getTwilightEvening().toString());
+    }
+
+    private void loadMoonRelatedData() {
+        AstroCalculator.MoonInfo moonInfo = astroCalculator.getMoonInfo();
+        final TextView moonriseDate = findViewById(R.id.moonRise);
+        final TextView moonsetDate = findViewById(R.id.moonSet);
+        final TextView nextNewMoon = findViewById(R.id.nextNewMoon);
+        final TextView nextFullMoon = findViewById(R.id.nextFullMoon);
+        final TextView illumination = findViewById(R.id.illumination);
+        final TextView monthAge = findViewById(R.id.monthAge);
+        moonriseDate.setText(moonInfo.getMoonrise().toString());
+        moonsetDate.setText(moonInfo.getMoonset().toString());
+        nextNewMoon.setText(moonInfo.getNextNewMoon().toString());
+        nextFullMoon.setText(moonInfo.getNextFullMoon().toString());
+        illumination.setText(String.valueOf(moonInfo.getIllumination()));
+        monthAge.setText(String.valueOf(moonInfo.getAge()));
     }
 
     @Override
